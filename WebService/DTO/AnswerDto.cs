3cfac94b62d990d51ae43f1c8e26e434ae3f1d10
Yohannes Kidane <joekidane7@gmail.com>
2018-11-05 10:59:47 +0100

    All the dataservice test are working properly and 5 of the web service also passed.

---
+++ WebService/DTO/AnswerDto.cs
@@ -0,0 +1,18 @@
+﻿using DataLayer.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebService.DTO
+{
+    public class AnswerDto 
+    {
+        public string Url { get; set; }
+        public int Score { get; set; }
+        public string Body { get; set; }
+        public string UrlComment { get; set; }
+
+
+    }
+}
